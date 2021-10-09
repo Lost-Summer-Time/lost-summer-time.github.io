6d4e51ec9aad72c89f28428252ed2b2a00bdedca
NiktoX2 <69085963+NiktoX2@users.noreply.github.com>
2021-10-09 14:44:36 +0300

    09.10.2021.14.44

--- js/sizeFix.js
+++
@@ -1,8 +0,0 @@
-function fixSize(){
-    let contentTextOne = document.getElementById("contentTextOne").scrollHeight;
-    let contentTextTwo = document.getElementById("contentTextTwo").scrollHeight;
-    let contentTextThree = document.getElementById("contentTextThree").scrollHeight;
-    body.style.minHeight=contentTextOne+contentTextTwo+contentTextThree+80+"px"
-}
-fixSize();
-window.addEventListener("resize",()=>{fixSize()});
