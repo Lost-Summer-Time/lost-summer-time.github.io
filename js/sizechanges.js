1ec4dd3451dbb39bd559e1e3bda6e40b47e5f96e
NiktoX2 <69085963+NiktoX2@users.noreply.github.com>
2022-03-08 23:08:38 +0300

    08.03.2022 Fix x3

--- js/sizechanges.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("resize", () => {
-    if (document.body.clientWidth <= 360) {
-        console.log("Размер окна меньше/равен 360 - " + document.body.clientWidth);
-    } else {
-        console.log("Размер окна больше 360 - " + document.body.clientWidth);
-    }
-})
